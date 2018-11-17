981560091cc5007491ef87811278d5026e38d176
Kushal Mehta <kushalmehta13@gmail.com>
2018-11-17 17:50:14 -0500

    Created 3 more servers. There are now 4 servers in total

---
+++ calc2/src/main/java/com/os/calc2/calcEndpoint.java
@@ -0,0 +1,37 @@
+package com.os.calc2;
+
+
+import org.example.calculatordescription.GetDifferenceRequest;
+import org.example.calculatordescription.GetDifferenceResponse;
+import org.example.calculatordescription.GetSquareRequest;
+import org.example.calculatordescription.GetSquareResponse;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+@Endpoint
+public class calcEndpoint {
+	
+	@PayloadRoot(namespace = "http://www.example.org/calculatorDescription", localPart = "getSquareRequest")
+	@ResponsePayload
+	public GetSquareResponse getSquareRequest(@RequestPayload GetSquareRequest request){
+		
+		GetSquareResponse response = new GetSquareResponse();
+		int a = request.getVar1();
+		response.setSquare(a*a);
+		return response;
+	}
+	
+	@PayloadRoot(namespace = "http://www.example.org/calculatorDescription", localPart = "getDifferenceRequest")
+	@ResponsePayload
+	public GetDifferenceResponse getSumRequest(@RequestPayload GetDifferenceRequest request){
+		
+		GetDifferenceResponse response = new GetDifferenceResponse();
+		int a = request.getVar1();
+		int b = request.getVar2();
+		response.setDifference(a-b);
+		return response;
+	}
+	
+}
---
+++ calc4/src/main/java/com/os/calc4/Calc4Application.java
@@ -0,0 +1,12 @@
+package com.os.calc4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Calc4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Calc4Application.class, args);
+	}
+}
